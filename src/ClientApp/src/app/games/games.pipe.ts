aaf9cc8d2409563b5390373496364032d23f89e7
Eric Hill <erichill.net@gmail.com>
2020-08-21 19:12:00 -0400

    fix games filter check

--- src/ClientApp/src/app/games/games.pipe.ts
+++ src/ClientApp/src/app/games/games.pipe.ts
@@ -7,7 +7,11 @@ import { Game } from './games.service';
 export class FilterOnNamePipe implements PipeTransform {
 
   transform(games: Game[], substring: string) {
-    return games && games.filter(game => !game.name || game.name.toLowerCase().startsWith(substring));
+    return games && games.filter(game => !game.name || this.startsWith(game.name, substring));
+  }
+
+  private startsWith(string1: string, string2: string) {
+    return string1.toLowerCase().startsWith(string2.toLowerCase());
   }
 
 }
